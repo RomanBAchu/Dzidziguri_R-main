11307ade28e18566ac4ca8e3b0e03b7d339c8af0
romanbachu <romanbachu@gmail.com>
2025-06-29 12:36:37 +0300

    add using static System.Console;

--- EmptyProj/Assets/Scripts/EmptyScript.cs
+++ EmptyProj/Assets/Scripts/EmptyScript.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using static System.Console;
+
 public class Scripts : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
