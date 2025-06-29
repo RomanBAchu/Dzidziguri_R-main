6d73a078852b39f2de960424bab82e348b6faf73
romanbachu <romanbachu@gmail.com>
2025-06-29 12:39:33 +0300

    add debug log in script

--- EmptyProj/Assets/Scripts/EmptyScript.cs
+++ EmptyProj/Assets/Scripts/EmptyScript.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using static System.Console;
 
 public class Scripts : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Working");
 
     }
 
